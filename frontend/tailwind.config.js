6b13411b1b6abac5becafcb51aac92540ccb0cbf
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-12 17:24:50 +0200

    added deleted colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        f1Red: '#FF1E00',  // Formula 1 inspired red
+        f1Grey: '#2E2E2E', // Dark grey for background
+        f1Highlight: '#D0021B', // Red for menu highlight
+        f1Green: '#00D369', // Green for lap time improvements
+        f1Purple: '#9B30FF', // Purple for fastest lap
+        f1DeltaRed: '#FF0000', // Red for slower lap delta
+        // Team Colors
         mercedes: '#00D2BE',  // Mercedes - Teal Green
         ferrari: '#DC0000',   // Ferrari - Red
         redbull: '#1E41FF',   // Red Bull Racing - Navy Blue
