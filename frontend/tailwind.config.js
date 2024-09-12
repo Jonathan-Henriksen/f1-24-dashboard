4a5b5d66a31421e2d4b524b11e048e4ae5604843
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-12 17:34:19 +0200

    corrected color codes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,23 +6,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        f1Red: '#FF1E00',  // Formula 1 inspired red
-        f1Grey: '#2E2E2E', // Dark grey for background
-        f1Highlight: '#D0021B', // Red for menu highlight
-        f1Green: '#00D369', // Green for lap time improvements
-        f1Purple: '#9B30FF', // Purple for fastest lap
-        f1DeltaRed: '#FF0000', // Red for slower lap delta
+        f1Red: '#FF1E00',
+        f1Grey: '#2E2E2E',
+        f1Highlight: '#D0021B',
+        f1Green: '#00D369',
+        f1Purple: '#9B30FF',
+        f1DeltaRed: '#FF0000',
         // Team Colors
-        mercedes: '#00D2BE',  // Mercedes - Teal Green
-        ferrari: '#DC0000',   // Ferrari - Red
-        redbull: '#1E41FF',   // Red Bull Racing - Navy Blue
-        williams: '#005AFF',  // Williams - Light Blue
-        astonmartin: '#006F62', // Aston Martin - British Racing Green
-        alpine: '#0090FF',    // Alpine - Blue
-        rb: '#1E41FF',        // RB (previously AlphaTauri) - Navy Blue
-        haas: '#FFFFFF',      // Haas - White
-        mclaren: '#FF8700',   // McLaren - Papaya Orange
-        sauber: '#00A08C',    // Sauber (previously Alfa Romeo) - Green
+        mercedes: '#27F4D2',
+        ferrari: '#E7002D',
+        redbull: '#3671C6',
+        williams: '#64C4FF',
+        astonmartin: '#229971',
+        alpine: '#FF87BC',
+        rb: '#6692FF',
+        haas: '#B6BABD',
+        mclaren: '#FF8000',
+        sauber: '#52E252',
       },
     },
   },
