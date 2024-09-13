36cdeef1b0d1b810b795f2b193dc86f4c99c49b7
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-13 20:44:26 +0200

    again

--- database/models/multi_functional_display.py
+++ database/models/multi_functional_display.py
@@ -81,7 +81,9 @@ class MultiFunctionDisplay:
 		self.leaderboard_panel().update_from_lap_data_packet(lap_data_packet)
 
 		player = self.leaderboard_panel().get_driver_by_position(player_data.car_position)
-		teammate = next(driver for driver in self.leaderboard_panel().drivers.values() if player and driver.team == player.team and not driver.is_player)
+		teammate = None
+		if player is not None:
+			teammate = next(driver for driver in self.leaderboard_panel().drivers.values() if driver.team == player.team and not driver.is_player)
 		driver_lead = self.leaderboard_panel().get_driver_by_position(1)
 		driver_in_front = self.leaderboard_panel().get_driver_by_position(player_data.car_position - 1)
 		driver_behind = self.leaderboard_panel().get_driver_by_position(player_data.car_position + 1)
