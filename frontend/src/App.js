fffe415803736560d5b34a5b5542bfd82405b218
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-14 20:21:56 +0200

    trying more general styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -41,11 +41,11 @@ function App() {
 	};
 
 	return (
-		<div className="inline-flex flex-col h-dvh w-screen px-6 pt-4 pb-6 bg-f1Grey text-white">
-			<div className="shadow-md rounded-lg mb-4 h-1/6">
+		<div className="inline-flex flex-col h-dvh w-screen pt-4 pb-6 bg-f1Grey text-white">
+			<div className="shadow-md rounded-xl mx-8 mb-4 h-1/6">
 				<Navigation panels={mfdData.panels} activePanelIndex={mfdData.active_panel_index} />
 			</div>
-			<div className="bg-gray-800 shadow-md rounded-lg mt-4 p-4 h-full">
+			<div className="bg-gray-800 shadow-md rounded-xl mt-4 p-4 h-full">
 				{renderActivePanel()}
 			</div>
 		</div>
