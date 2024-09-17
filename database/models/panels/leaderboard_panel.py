88f84974f1342b519a0bc5135b277a3bac5a56e4
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-17 20:12:01 +0200

    minor

--- database/models/panels/leaderboard_panel.py
+++ database/models/panels/leaderboard_panel.py
@@ -44,9 +44,10 @@ class LeaderboardPanel():
 				player_current_data = self.get_driver_by_position(player_lap_data.car_position)
 				other_driver_current_data = self.get_driver_by_position(lap_data.car_position)
 
-				delta_to_player_in_ms = abs(other_driver_current_data.lap_time_personal_best.to_milliseconds() - player_current_data.lap_time_personal_best.to_milliseconds())
-				
-				self.drivers[car_index].delta_to_player = Time(ms_part=delta_to_player_in_ms)
+				if player_current_data.lap_time_personal_best and other_driver_current_data.lap_time_personal_best:
+					delta_to_player_in_ms = abs(other_driver_current_data.lap_time_personal_best.to_milliseconds() - player_current_data.lap_time_personal_best.to_milliseconds())
+					
+					self.drivers[car_index].delta_to_player = Time(ms_part=delta_to_player_in_ms)
 			else:
 				player_delta_to_race_leader = Time(minutes_part=player_lap_data.delta_to_race_leader_minutes_part, ms_part=player_lap_data.delta_to_race_leader_ms_part)
 				other_driver_delta_to_race_leader = Time(minutes_part=lap_data.delta_to_race_leader_minutes_part, ms_part=lap_data.delta_to_race_leader_ms_part)
