a83dac0609c27ae70c25b2f19233a56d3a293b4a
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-17 19:57:00 +0200

    Tyre Info panel initial

--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -1,28 +1,13 @@
 import React from "react";
-import TyreInfoHeader from "./TyreInfoHeader";
-import TyreInfo from "./TyreInfo";
-import { getCarTopViewImage } from "helpers/helpers";
-import "./TyreInfoPanel.css";
 
-const TyreInfoPanel = ({ data, teamName }) => {
+const TyreInfoPanel = ({ panelData, teamName }) => {
 	return (
 		<div className="tyre-info-panel">
-			{/* General Info (Top Row) */}
-			<TyreInfoHeader
-				compound={data.tyre_compound_visual}
-				wearPercentage={data.tyre_set_total_wear_percentage}
-			/>
-
-			{/* Car Image with Tyre Details (Bottom Row) */}
-			<div className="tyre-info-car-container">
-				<img src={getCarTopViewImage(teamName)} alt="Car top view" className="tyre-info-car-image" />
-
-				{/* Tyre Details rendered in respective positions */}
-				<TyreInfo tyre={data.tyre_front_left} position="front-left" />
-				<TyreInfo tyre={data.tyre_front_right} position="front-right" />
-				<TyreInfo tyre={data.tyre_rear_left} position="rear-left" />
-				<TyreInfo tyre={data.tyre_rear_right} position="rear-right" />
-			</div>
+			<div className="tyre-info-temps front left"></div>
+			<div className="tyre-info-temps front right"></div>
+			<div className="tyre-info-car-image-container"></div>
+			<div className="tyre-info-temps rear right"></div>
+			<div className="tyre-info-temps rear right"></div>
 		</div>
 	);
 };
