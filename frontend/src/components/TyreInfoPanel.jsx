eb1d6875cf7f9b7820d005e6b14f772de8562013
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-18 13:40:02 +0200

    last try

--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const TyreInfoPanel = ({ panelData, teamName }) => {
 	return (
-		<div className="grid grid-cols-8 grid-rows-8 gap-4 py-4 aspect-square h-full">
+		<div className="grid grid-cols-8 grid-rows-8 gap-8 p-8">
 			<div className="col-start-2 col-span-6 row-start-1 row-span-2"></div>
 
 			{/* Front Left Temperatures */}
-			<div className="col-start-2 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25">
+			<div className="col-start-2 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25 aspect-square">
 				<div className="flex grow flex-col justify-start rounded-full p-8 border-2 bg-f1Green border-mainBorder/25">
 					<span>88C</span>
 					<div className="grow rounded-full p-8 border-2 bg-mainBlue/80 border-mainBorder/25">
