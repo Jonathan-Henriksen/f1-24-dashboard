c97e034f82fe295a050a67c95a0a179579867d80
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-18 15:26:02 +0200

    d

--- frontend/src/components/TyreInfoCircle.jsx
+++ frontend/src/components/TyreInfoCircle.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const TyreInfoCircle = ({ tyre }) => {
 	return (
-		<div className="flex flex-col justify-start rounded-full p-8 border-2 bg-f1Green border-mainBorder/25 aspect-square">
+		<div className="flex flex-col justify-start rounded-full p-8 border-2 bg-f1Green border-mainBorder aspect-square">
 			<span>{tyre.temperature_surface}C</span>
-			<div className="flex flex-col justify-start rounded-full p-8 border-4 bg-f1Green border-mainBorder/25">
+			<div className="flex flex-col justify-start rounded-full p-8 border-4 bg-f1Green border-mainBorder aspect-square">
 				<span>{<span>{tyre.temperature_carcass}C</span>}C</span>
-				<div className="grow rounded-full p-8 border-4 bg-mainBlue/80 border-mainBorder/25">
+				<div className="grow rounded-full p-8 border-4 bg-mainDark/50 border-mainBorder aspect-square">
 					<span>{Math.trunc(tyre.wear_percentage)}%</span>
 				</div>
 			</div>
