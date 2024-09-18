91531c0ddbde5340fd5b79f3da9ad853b377e9f4
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-18 13:24:59 +0200

    mby

--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -3,11 +3,11 @@ import React from "react";
 
 const TyreInfoPanel = ({ panelData, teamName }) => {
 	return (
-		<div className="grow grid grid-cols-8 grid-rows-8 gap-4 px-8 py-4">
-			<div className="col-start-2 col-span-6 row-start-1 row-span-2"></div>
+		<div className="grow grid grid-cols-16 grid-rows-8 gap-8 p-8">
+			<div className="col-start-4 col-span-12 row-start-1 row-span-2"></div>
 
 			{/* Front Left Temperatures */}
-			<div className="col-start-2 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25">
+			<div className="col-start-4 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25">
 				<div className="flex grow flex-col justify-start rounded-full p-8 border-2 bg-f1Green border-mainBorder/25">
 					<span>88C</span>
 					<div className="grow rounded-full p-8 border-2 bg-mainBlue/80 border-mainBorder/25">
@@ -16,15 +16,15 @@ const TyreInfoPanel = ({ panelData, teamName }) => {
 				</div>
 			</div>
 
-			<div className="col-start-6 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
+			<div className="col-start-12 row-start-3 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
 
 			{/* Car Image */}
-			<div className="flex justify-center grow col-start-4 col-span-2 row-start-3 row-span-6">
+			<div className="flex justify-center grow col-start-8 col-span-2 row-start-3 row-span-6">
 				<img src={getCarTopViewImage(teamName)} alt={teamName} className="max-w-full" />
 			</div>
 
-			<div className="col-start-2 row-start-7 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
-			<div className="col-start-6 row-start-7 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
+			<div className="col-start-4 row-start-7 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
+			<div className="col-start-12 row-start-7 col-span-2 row-span-2 border-2 bg-mainDark/50  border-mainBorder/25"></div>
 		</div>
 	);
 }
