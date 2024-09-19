090cdac2749f854feaf35fd489aeb37e4a964738
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 16:21:09 +0200

    d

--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -27,7 +27,6 @@ const TyreInfoPanel = ({ data, teamName }) => {
 			<div className="flex flex-col justify-center w-10 items-center p-2 border-2 bg-mainDark/50 border-mainBorder/25">
 				{tempRange().map((temp, index) => (
 					<div key={index} className={"flex grow px-3 py-1"} style={{ backgroundColor: getColorFromList(colorRange, temp) }}>
-						<p>Test</p>
 					</div>
 				))}
 			</div>
