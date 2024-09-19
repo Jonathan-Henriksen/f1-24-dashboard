beaf6f800e7e1cd928bda434de6d314e33569384
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 17:23:29 +0200

    d

--- frontend/src/components/TyreInfoCircle.jsx
+++ frontend/src/components/TyreInfoCircle.jsx
@@ -22,7 +22,7 @@ const TyreInfoCircle = ({ colorRange, tyre }) => {
 					>
 						<CircularProgressBar
 							chartValue={{
-								[tyre.wear_percentage]: '#ff00ff00',
+								[tyre.wear_percentage]: '#282927',
 								'100': getColorFromList(colorRange, tyre.temperature_carcass)
 							}}
 							styles="pie-chart"
