4d632d5e6bbea29e4f8dacf1e28be3e55360d971
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 17:14:51 +0200

    Revert "d"
    This reverts commit 6be361004b963687ed5ce67c75e55eab069f8738.

--- frontend/src/components/TyreInfoCircle.jsx
+++ frontend/src/components/TyreInfoCircle.jsx
@@ -21,10 +21,17 @@ const TyreInfoCircle = ({ colorRange, tyre }) => {
 						size="3rem"
 					>
 						<CircularProgressBar
-							color={getColorFromList(colorRange, tyre.temperature_carcass)}
+							chartValue={{
+								[tyre.wear_percentage]: 'transparent',
+								'100': getColorFromList(colorRange, tyre.temperature_carcass)
+							}}
 							styles="pie-chart"
-							percentage={tyre.wear_percentage}
-							showPercentage={true}
+							text={`${tyre.temperature_carcass}°C`}
+							textStyle={{
+								color: 'white',
+								fontSize: '2rem',
+								fontStyle: 'bold'
+							}}
 						/>
 					</CircularProgressBar>
 				</CircularProgressBar >
