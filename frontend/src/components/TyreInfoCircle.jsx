fba8c785465037832a8f464bd690fab56c6d517d
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 23:21:48 +0200

    d

--- frontend/src/components/TyreInfoCircle.jsx
+++ frontend/src/components/TyreInfoCircle.jsx
@@ -3,7 +3,7 @@ import { getColorFromList } from "helpers/colorHelper";
 import React from "react";
 
 const TyreData = ({ tyre }) => (
-	<div className="flex justify-center pr-2 divide-x-2 gap-4 divide-mainWhite/50">
+	<div className="flex justify-center pr-4 divide-x-2 gap-4 divide-mainWhite/50">
 
 		{/* Titles */}
 		<div className="flex flex-col justify-start justify-items-stretch">
@@ -13,7 +13,7 @@ const TyreData = ({ tyre }) => (
 			<span className="grow text-center text-xl uppercase">Wear</span>
 		</div>
 		{/* Values */}
-		<div className="flex flex-col justify-start justify-items-stretch">
+		<div className="flex flex-col justify-start justify-items-stretch pl-2">
 			<span className="grow text-center text-lg font-bold">{tyre.temperature_brakes}°C</span>
 			<span className="grow text-center text-lg font-bold">{tyre.temperature_carcass}°C</span>
 			<span className="grow text-center text-lg font-bold">{tyre.temperature_surface}°C</span>
@@ -51,18 +51,24 @@ const TyreGraphic = ({ colorRange, tyre }) => (
 	</div>
 )
 
-const TyreInfoCircle = ({ colorRange, tyre }) => {
-	console.log(`Color Range = ${colorRange}`)
+const TyreInfoCircle = ({ colorRange, tyre, inverted = false }) => {
+	console.log(`Color Range = ${colorRange}`);
 	return (
 		<div className="flex justify-center p-4 border-2 rounded-xl shadow-lg bg-mainDark/50 border-mainBorder/25 shadow-mainDark/75">
-
-			{/* Tyre Data */}
-			<TyreData tyre={tyre} />
-
-			{/* Tyre Graphic */}
-			<TyreGraphic tyre={tyre} colorRange={colorRange} />
+			{inverted ? (
+				<>
+					<TyreGraphic tyre={tyre} colorRange={colorRange} />
+					<TyreData tyre={tyre} />
+				</>
+			) : (
+				<>
+					<TyreData tyre={tyre} />
+					<TyreGraphic tyre={tyre} colorRange={colorRange} />
+				</>
+			)}
 		</div>
 	);
 };
+
 
 export default TyreInfoCircle;
