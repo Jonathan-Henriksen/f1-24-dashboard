c61dffec1015cfeea449bb49101ad2dbb9bdcd2b
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 15:49:02 +0200

    legend

--- frontend/src/components/TyreInfoCircle.jsx
+++ frontend/src/components/TyreInfoCircle.jsx
@@ -2,14 +2,7 @@ import { CircularProgressBar } from "react-percentage-bar";
 import { getColorFromList } from "helpers/colorHelper";
 import React from "react";
 
-const colorRange = [
-	{ color: '#3EBEF7', value: 40 },  // Blue at 40
-	{ color: '#2BDD1A', value: 85 },  // Green at 85
-	{ color: '#FBCD4C', value: 95 },  // Yellow at 95
-	{ color: '#EC3B26', value: 115 }  // Red at 110
-];
-
-const TyreInfoCircle = ({ tyre }) => {
+const TyreInfoCircle = ({ colorRange, tyre }) => {
 	return (
 		<div className="flex justify-center p-4 border-2 rounded-xl shadow-lg bg-mainDark/50 border-mainBorder/25 shadow-mainDark/75">
 			<div className="opacity-75">
--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -1,19 +1,51 @@
 import React from "react";
 import { getCarTopViewImage } from "helpers/helpers";
 import TyreInfoCircle from "./TyreInfoCircle";
+import { getColorFromList } from "helpers/colorHelper";
+
+const colorRange = [
+	{ color: '#3EBEF7', value: 40 },  // Blue at 40
+	{ color: '#2BDD1A', value: 85 },  // Green at 85
+	{ color: '#FBCD4C', value: 95 },  // Yellow at 95
+	{ color: '#EC3B26', value: 115 }  // Red at 110
+];
+
+const tempRange = () => {
+	const arr = [];
+	for (let i = 40; i <= 115; i++) {
+		arr.push(i);
+	}
+
+	return arr;
+}
+
+const LegendElement = (value, index) => {
+	let color = getColorFromList(colorRange, value)
+	return (
+		<div key={index} className={`flex h-1 w-4 bg-${color}`}>
+		</div>
+	)
+}
 
 const TyreInfoPanel = ({ data, teamName }) => {
 	return (
 		<div className="flex grow justify-center p-12">
 
+			{/* Color Legend */}
+			<div className="flex flex-col justify-center items-center p-2 border-2 bg-mainDark/50 border-mainBorder/25">
+				{tempRange().map((temp, index) => (
+					<LegendElement in value={temp} index={index} />
+				))}
+			</div>
+
 			{/* Left Tyres */}
 			<div className="flex flex-col justify-between">
 
 				{/* Front */}
-				<TyreInfoCircle tyre={data.tyre_front_left} />
+				<TyreInfoCircle tyre={data.tyre_front_left} colorRange={colorRange} />
 
 				{/* Rear */}
-				<TyreInfoCircle tyre={data.tyre_rear_left} />
+				<TyreInfoCircle tyre={data.tyre_rear_left} colorRange={colorRange} />
 			</div>
 
 			{/* Car Image */}
@@ -25,10 +57,10 @@ const TyreInfoPanel = ({ data, teamName }) => {
 			<div className="flex flex-col justify-between">
 
 				{/* Front */}
-				<TyreInfoCircle tyre={data.tyre_front_right} />
+				<TyreInfoCircle tyre={data.tyre_front_right} colorRange={colorRange} />
 
 				{/* Rear */}
-				<TyreInfoCircle tyre={data.tyre_rear_right} />
+				<TyreInfoCircle tyre={data.tyre_rear_right} colorRange={colorRange} />
 			</div>
 		</div>
 	);
