b81188986447706f04264ce6608b7ae593febcdd
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-19 16:09:27 +0200

    d

--- frontend/src/components/TyreInfoPanel.jsx
+++ frontend/src/components/TyreInfoPanel.jsx
@@ -26,7 +26,7 @@ const TyreInfoPanel = ({ data, teamName }) => {
 			{/* Color Legend */}
 			<div className="flex flex-col justify-center w-10 items-center p-2 border-2 bg-mainDark/50 border-mainBorder/25">
 				{tempRange().map((temp, index) => (
-					<div key={index} className={`flex w-max h-2 border-[${getColorFromList(colorRange, temp)}] bg-[${getColorFromList(colorRange, temp)}]`}>
+					<div key={index} className={`min-w-max h-2 border-[${getColorFromList(colorRange, temp)}] bg-[${getColorFromList(colorRange, temp)}]`}>
 					</div>
 				))}
 			</div>
