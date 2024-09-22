39ac46d96323a8f3351ee938cc81508cf86e19ae
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-22 16:31:46 +0200

    fixed width test

--- frontend/src/components/FocusPanel.jsx
+++ frontend/src/components/FocusPanel.jsx
@@ -10,25 +10,25 @@ import React from "react";
 const TimingsCard = ({ timingsData }) => (
 	<Card flex="flex items-center divide-x-2 divide-mainBorder/50">
 		{/* Fastest Lap*/}
-		<div className="flex flex-col p-4 gap-2">
+		<div className="flex flex-col p-4 gap-2 w-1/4">
 			<span className="text-center text-4xl tracking-wide">Fastest Lap</span>
 			<span className="text-center text-5xl font-semibold tracking-wide text-lapTime-purple/90">{formatTime(timingsData.lap_time_fastest_driver.lap_time_personal_best)}</span>
 		</div>
 
 		{/* Personal Best*/}
-		<div className="flex flex-col p-4 gap-2">
+		<div className="flex flex-col p-4 gap-2 w-1/4">
 			<span className="text-center text-4xl tracking-wide">Personal Best</span>
 			<span className="text-center text-5xl font-semibold tracking-wide text-lapTime-green/90">{formatTime(timingsData.lap_time_personal_best)}</span>
 		</div>
 
 		{/* Teammate Best*/}
-		<div className="flex flex-col p-4 gap-2">
+		<div className="flex flex-col p-4 gap-2 w-1/4">
 			<span className={"text-center text-4xl"}>Teammate Best</span>
 			<span className={`text-center text-5xl font-semibold tracking-wide ${timingsData.player ? getTeamColor(timingsData.player.team) : ''}`}>{formatTime(timingsData.lap_time_teammate_best)}</span>
 		</div>
 
 		{/* Current Lap*/}
-		<div className="flex flex-col p-4 gap-2">
+		<div className="flex flex-col p-4 gap-2 w-1/4">
 			<span className="text-center text-4xl">Current Lap</span>
 			<span className={`text-center text-5xl font-semibold tracking-wide ${timingsData.lap_time_current_invalidated ? 'text-mainRed' : ''}`}>{formatTime(timingsData.lap_time_current)}</span>
 		</div>
