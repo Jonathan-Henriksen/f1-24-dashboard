3a7a551288949c7594126b0fd063f352a2a900cf
Jonathan Henriksen <jonathan.henriksen@akqa.com>
2024-09-27 01:33:02 +0200

    d

--- frontend/src/components/cards/TimeLeftCard.jsx
+++ frontend/src/components/cards/TimeLeftCard.jsx
@@ -7,7 +7,7 @@ const TimeLeftCard = ({ seconds }) => {
 	const remainingSeconds = seconds % 60;
 
 	return (
-		<Card flex="flex flex-col basis-1/5 justify-center min-h-32 max-h-44">
+		<Card flex="flex flex-auto flex-col basis-1/5 justify-center h-full min-h-32 max-h-44">
 			<span className="text-center text-3xl font-bold tracking-wide text-mainWhite/80">Time Left</span>
 			<span className={`text-center text-5xl font-semibold tracking-wide ${minutes < 2 ? 'text-mainRed' : ''}`}>{formatTime({ minutes: minutes, seconds: remainingSeconds, ms: 0 }, { excludeMs: true })}</span>
 		</Card>
